74d8cdc568ed858a38bcc8aef23bb04918b6d447
justinlucky <84560254+justinlucky@users.noreply.github.com>
2022-12-27 01:52:34 +0530

    Add files via upload

---
+++ sidebar/sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './sidebar.css';
+const Sidebar = () => {
+  return (
+    <div id='sidebar_container'>
+        <div className='link'>Facebook</div>
+        <div className='link'>Twitter</div>
+        <div className='link'>Youtube</div>
+        <div className='link'>Instagram</div>
+        <div className='link'>Whatsapp</div>
+    </div>
+  )
+}
+
+export default Sidebar
